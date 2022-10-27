d5ec39a8ff73207c8746bf1d70d25ed57129d265
Paul Fletes <paul.fletes@gmail.com>
2022-10-27 00:42:13 -0500

    define deposit method

--- bank_account.py
+++ bank_account.py
@@ -9,3 +9,8 @@ class BankAccount:
         self.full_name = full_name
         self.account_number = random.randint(80000000, 89999999)
         self.balance = 0
+
+    # methods defined below: deposit, withdraw, get_balance, add_interest, print_statement
+    def deposit(self, amount):
+        self.balance += amount
+        print(f'Amount deposited: ${amount}. New balance: ${self.balance}')
