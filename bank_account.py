7aa6b39245e14a99409e57671375c0e31ef56b54
Paul Fletes <paul.fletes@gmail.com>
2022-10-27 20:46:00 -0500

    created 3 instances of BankAccount class

--- bank_account.py
+++ bank_account.py
@@ -7,10 +7,10 @@ import random
 class BankAccount:
     def __init__(self, full_name):
         self.full_name = full_name
-        self.account_number = random.randint(80000000, 89999999)
+        self.account_number = random.randint(80000000, 99999999)
         self.balance = 0
 
-    # methods defined below: deposit, withdraw, get_balance, add_interest, print_statement
+# methods defined below: deposit, withdraw, get_balance, add_interest, print_statement
     def deposit(self, amount):
         self.balance += amount
         print(f'Amount deposited: ${amount}. New balance: ${self.balance}')
@@ -32,3 +32,34 @@ class BankAccount:
             interest = (self.balance * 0.00083)
             self.balance += interest
             self.balance = round(self.balance, 2)
+
+    def print_statement(self):
+        secure_account = str(self.account_number)
+        secure_account = (secure_account[4:8])
+        secure_account = f'****{secure_account}'
+        print(
+            f'{self.full_name}\nAccount Number: {secure_account}\nBalance: {self.balance}')
+
+
+# 3 instances of BankAccount class below:
+mitchell_account = BankAccount('Mitchell')
+mitchell_account.deposit(400000)
+mitchell_account.print_statement()
+mitchell_account.add_interest()
+mitchell_account.print_statement()
+mitchell_account.withdraw(150)
+mitchell_account.print_statement()
+
+intern_account = BankAccount('Paul')
+intern_account.deposit(1400)
+intern_account.add_interest()
+intern_account.print_statement()
+intern_account.withdraw(1450)
+intern_account.print_statement()
+
+dev_account = BankAccount('Mark')
+dev_account.deposit(100000)
+dev_account.add_interest()
+dev_account.print_statement()
+dev_account.withdraw(1000)
+dev_account.print_statement()
